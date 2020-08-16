d07c1984b3a4526e4bd3b70249b6662591c49196
James Sartelle <james.sartelle@moodmedia.com>
2020-08-16 13:57:23 -0400

    Refactor saving into separate function

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ if (!fs.existsSync('./config/config.json')) {
 }
 
 const config = JSON.parse(fs.readFileSync('./config/config.json'));
-
 const twitter = new Twitter(config.auth);
+
 const lists = Object.entries(config.lists);
 
 if (!fs.existsSync('./config/listInfo.json')) {
@@ -48,106 +48,21 @@ const listsPromise = Promise.all(lists.map(async ([listId, listOptions]) => {
             });
         }
 
-        const allowedMediaTypes = {
-            /* jshint ignore:start */
-            "photo": listOptions?.mediaTypes?.photo ?? true,
-            "video": listOptions?.mediaTypes?.video ?? true,
-            "animated_gif": listOptions?.mediaTypes?.animated_gif ?? true
-            /* jshint ignore:end */
-        };
-
-        const statusesConfig = {
+        const requestConfig = {
             list_id: listId,
             include_rts: listOptions.retweets,
             count: 1000,
             tweet_mode: "extended"
         };
         if (!listOptions.ignoreLatestTweetId && listInfo[listId].latestTweetId) {
-            statusesConfig.since_id = listInfo[listId].latestTweetId;
+            requestConfig.since_id = listInfo[listId].latestTweetId;
         }
-        const statuses = await twitter.get('lists/statuses', statusesConfig);
+        const statuses = await twitter.get('lists/statuses', requestConfig);
 
         console.debug(`Got ${statuses.length} tweets in list ${listInfo[listId].name}`);
 
-        const baseDir = listOptions?.paths?.output ?? `./out/${listInfo[listId].name}/`; // jshint ignore:line
-        const retweetDir = listOptions?.paths?.retweets ?? path.join(baseDir, 'retweets'); // jshint ignore:line
-
-        let latestTweetDate, latestTweetId;
-        let retweetLog = '';
-
-        statuses.forEach(status => {
-            const isRetweet = Boolean(status.retweeted_status || status.quoted_status);
-            const tweet = status.retweeted_status || status.quoted_status || status;
-
-            const tweetDate = luxon.DateTime.fromFormat(tweet.created_at, 'EEE MMM dd HH:mm:ss ZZZ yyyy');
-            if (!latestTweetDate || tweetDate > latestTweetDate) {
-                latestTweetDate = tweetDate;
-                latestTweetId = tweet.id_str;
-            }
-
-            if (tweet.extended_entities && tweet.extended_entities.media) {
-                tweet.extended_entities.media.forEach((media, index) => {
-                    if (allowedMediaTypes[media.type]) {
-                        const dir = isRetweet ? retweetDir : baseDir;
-
-                        const base = `${tweet.user.screen_name}_${tweetDate.toISODate()}_${tweet.id_str}_${index + 1}`;
-
-                        let ext, url;
-                        switch (media.type) {
-                            case 'photo':
-                                ext = path.extname(media.media_url_https);
-                                url = media.media_url_https + ':orig';
-                                break;
-                            case 'video':
-                            case 'animated_gif':
-                                ext = '.mp4';
-                                url = media.video_info.variants.reduce((prev, curr) => {
-                                    if (
-                                        curr.content_type !== 'video/mp4' ||
-                                        curr.bitrate <= prev.bitrate
-                                    ) return prev;
-                                    return curr;
-                                }, media.video_info.variants[0]).url;
-                                break;
-                        }
-                        if (listOptions.dryRun) ext += '_blank';
-
-                        if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
-
-                        const baseDestinationPath = path.join(baseDir, base + ext);
-                        const destinationPath = path.join(dir, base + ext);
-
-                        // do not save retweeted media if it already exists in the base folder
-                        if (!fs.existsSync(baseDestinationPath) && !fs.existsSync(destinationPath)) {
-                            if (listOptions.dryRun) {
-                                fs.writeFile(destinationPath, '', (err) => {
-                                    if (err) console.warn(err);
-                                });
-
-                            } else {
-                                const stream = fs.createWriteStream(destinationPath);
-                                https.get(url, res => {
-                                    res.pipe(stream);
-                                    stream.on('finish', stream.end);
-                                    stream.on('error', err => console.warn(err));
-
-                                }).on('error', err => {
-                                    console.warn(err);
-                                });
-                            }
-
-                            if (isRetweet) {
-                                retweetLog += `${status.user.screen_name} : ${base + ext}\n`;
-                            }
-                        }
-                    }
-                });
-            }
-        });
-
-        if (latestTweetId) listInfo[listId].latestTweetId = latestTweetId;
-
-        if (listOptions.logRetweets) fs.appendFileSync(path.join(retweetDir, '_retweets.txt'), retweetLog);
+        const { latestTweetId } = saveStatuses(statuses, listInfo[listId].name, listOptions);
+        listInfo[listId].latestTweetId = latestTweetId;
 
     } catch (err) {
         // TODO: better error handling
@@ -158,3 +73,97 @@ const listsPromise = Promise.all(lists.map(async ([listId, listOptions]) => {
 listsPromise.then(() => {
     fs.writeFileSync('./config/listInfo.json', JSON.stringify(listInfo, null, '\t'));
 });
+
+/* --- */
+
+function saveStatuses(statuses, folderName, options) {
+    const baseDir = options.paths?.output ?? `./out/${folderName}/`; // jshint ignore:line
+    const retweetDir = options.paths?.retweets ?? path.join(baseDir, 'retweets'); // jshint ignore:line
+
+    const allowedMediaTypes = {
+        /* jshint ignore:start */
+        "photo": options.mediaTypes?.photo ?? true,
+        "video": options.mediaTypes?.video ?? true,
+        "animated_gif": options.mediaTypes?.animated_gif ?? true
+        /* jshint ignore:end */
+    };
+
+    let latestTweetDate, latestTweetId;
+    let retweetLog = '';
+
+    statuses.forEach(status => {
+        const isRetweet = Boolean(status.retweeted_status || status.quoted_status);
+        const tweet = status.retweeted_status || status.quoted_status || status;
+
+        const tweetDate = luxon.DateTime.fromFormat(tweet.created_at, 'EEE MMM dd HH:mm:ss ZZZ yyyy');
+        if (!latestTweetDate || tweetDate > latestTweetDate) {
+            latestTweetDate = tweetDate;
+            latestTweetId = tweet.id_str;
+        }
+
+        if (tweet.extended_entities && tweet.extended_entities.media) {
+            tweet.extended_entities.media.forEach((media, index) => {
+                if (allowedMediaTypes[media.type]) {
+                    const dir = isRetweet ? retweetDir : baseDir;
+
+                    const base = `${tweet.user.screen_name}_${tweetDate.toISODate()}_${tweet.id_str}_${index + 1}`;
+
+                    let ext, url;
+                    switch (media.type) {
+                        case 'photo':
+                            ext = path.extname(media.media_url_https);
+                            url = media.media_url_https + ':orig';
+                            break;
+                        case 'video':
+                        case 'animated_gif':
+                            ext = '.mp4';
+                            url = media.video_info.variants.reduce((prev, curr) => {
+                                if (
+                                    curr.content_type !== 'video/mp4' ||
+                                    curr.bitrate <= prev.bitrate
+                                ) return prev;
+                                return curr;
+                            }, media.video_info.variants[0]).url;
+                            break;
+                    }
+                    if (options.dryRun) ext += '_blank';
+
+                    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+
+                    const baseDestinationPath = path.join(baseDir, base + ext);
+                    const destinationPath = path.join(dir, base + ext);
+
+                    // do not save retweeted media if it already exists in the base folder
+                    if (!fs.existsSync(baseDestinationPath) && !fs.existsSync(destinationPath)) {
+                        if (options.dryRun) {
+                            fs.writeFile(destinationPath, '', (err) => {
+                                if (err) console.warn(err);
+                            });
+
+                        } else {
+                            const stream = fs.createWriteStream(destinationPath);
+                            https.get(url, res => {
+                                res.pipe(stream);
+                                stream.on('finish', stream.end);
+                                stream.on('error', err => console.warn(err));
+
+                            }).on('error', err => {
+                                console.warn(err);
+                            });
+                        }
+
+                        if (isRetweet) {
+                            retweetLog += `${status.user.screen_name} : ${base + ext}\n`;
+                        }
+                    }
+                }
+            });
+        }
+    });
+
+    if (options.logRetweets) fs.appendFileSync(path.join(retweetDir, '_retweets.txt'), retweetLog);
+
+    return {
+        latestTweetId
+    };
+}
